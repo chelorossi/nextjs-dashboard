7722c55d20513ede629d42c2338ef4562af8e03c
Marcelo Rossi <marcerossi21@gmail.com>
2024-07-21 15:42:47 -0300

    Chapter 9: missing add Suspense to not block load

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,14 +1,13 @@
-import { Card } from "@/app/ui/dashboard/cards";
-import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import CardWrapper from "@/app/ui/dashboard/cards";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import { lusitana } from "@/app/ui/fonts";
-import { Suspense } from "react";
 import {
+  CardSkeleton,
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
 } from "@/app/ui/skeletons";
-import CardWrapper from "@/app/ui/dashboard/cards";
-import { CardSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
 
 export default async function Page() {
   return (
